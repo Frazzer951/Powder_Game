ee19a6b6511ac4a15995dbc1f5256bfd11bc4504
Luke Eltiste <lukeeltiste@csu.fullerton.edu>
2021-04-11 21:37:07 -0700

    Basic sand implementation

--- olcPowder_Game/main.cpp
+++ olcPowder_Game/main.cpp
@@ -15,26 +15,95 @@ public:
   bool OnUserCreate() override
   {
     powders = std::make_unique<powderType[]>( ScreenWidth() * ScreenHeight() );
-    for( int y = 0; y < ScreenWidth(); y++ )
+    for( int y = 0; y < ScreenHeight(); y++ )
     {
-      for( int x = 0; x < ScreenHeight(); x++ ) { powders[y * ScreenHeight() + x] = air; }
+      for( int x = 0; x < ScreenWidth(); x++ )
+      {
+        powders[y * ScreenWidth() + x] = air;
+        //if( x >= 95 && x <= 105 && y >= 95 && y < 105 ) powders[y * ScreenWidth() + x] = sand;
+        if( x == 100 && y >= 95 && y < 105 ) powders[y * ScreenWidth() + x] = sand;
+      }
     }
+
+    /*powders[99 * ScreenWidth() + 100]  = sand;
+    powders[100 * ScreenWidth() + 100] = sand;
+    powders[101 * ScreenWidth() + 100] = sand;
+    powders[102 * ScreenWidth() + 100] = sand;*/
 
     return true;
   }
 
   bool OnUserUpdate( float fElapsedTime ) override
   {
+    // Update powders
+    for( int y = ScreenHeight() - 1; y >= 0; y-- )
+    {
+      for( int x = 0; x < ScreenWidth(); x++ )
+      {
+        switch( powders[y * ScreenWidth() + x] )
+        {
+          case air:    // Do nothing
+            break;
+          case sand:    // Update Sand
+            moveSand( x, y );
+            break;
+          default: break;
+        }
+      }
+    }
+
     // Clear the screen
     Clear( olc::DARK_BLUE );
 
+    // Draw in powders
+    for( int y = 0; y < ScreenHeight(); y++ )
+    {
+      for( int x = 0; x < ScreenWidth(); x++ )
+      {
+        switch( powders[y * ScreenWidth() + x] )
+        {
+          case air:    // Do nothing
+            break;
+          case sand:    // Draw Boundary
+            Draw( x, y, olc::Pixel( 194, 178, 128 ) );
+            break;
+        }
+      }
+    }
+
     return true;
+  }
+
+  void moveSand( int x, int y )
+  {
+    // Check 3 positions and move accordingly
+
+    if( y + 1 >= ScreenWidth() ) return;
+
+    if( powders[( y + 1 ) * ScreenWidth() + x] == air )
+    {
+      // Check below
+      powders[( y + 1 ) * ScreenWidth() + x] = sand;
+      powders[y * ScreenWidth() + x]         = air;
+    }
+    else if( powders[( y + 1 ) * ScreenWidth() + ( x - 1 )] == air && x - 1 >= 0 )
+    {
+      // Check down left
+      powders[( y + 1 ) * ScreenWidth() + ( x - 1 )] = sand;
+      powders[y * ScreenWidth() + x]                 = air;
+    }
+    else if( powders[( y + 1 ) * ScreenWidth() + ( x + 1 )] == air && x + 1 < ScreenWidth() )
+    {
+      // Check down right
+      powders[( y - 1 ) * ScreenWidth() + ( x + 1 )] = sand;
+      powders[y * ScreenWidth() + x]                 = air;
+    }
   }
 };
 
 int main()
 {
   PowderGame game;
-  if( game.Construct( 500, 500, 2, 2 ) ) game.Start();
+  if( game.Construct( 200, 200, 4, 4 ) ) game.Start();
   return 0;
 }
