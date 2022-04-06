62e7eb336d1194e6c82bd9384307527d620f2a65
Luke Eltiste <luke343279@gmail.com>
2022-04-06 15:13:52 -0700

    format code

--- include/elements/Gas.h
+++ include/elements/Gas.h
@@ -8,15 +8,15 @@ class Gas : public Element
 {
 public:
   Gas( int x, int y ) : Element( x, y ) {}
-  void update(  PowderGame * pge, float fElapsedTime ) override
+  void update( PowderGame * pge, float fElapsedTime ) override
   {
     // Only update once per cycle
     if( updated == pge->isUpdated() ) { return; }
     updated = !updated;
   }
   void draw( PowderGame * pge ) override { pge->DrawElement( pos.x, pos.y, c ); }
-   bool actOnNeighboringElement( PowderGame * pge, Element * elem, int x, int y, bool isFinal, bool isFirst,
-                                     vec2i lastValid, int depth ) override
+  bool actOnNeighboringElement( PowderGame * pge, Element * elem, int x, int y, bool isFinal, bool isFirst,
+                                vec2i lastValid, int depth ) override
   {
     return true;
   }
